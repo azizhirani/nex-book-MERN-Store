dcb20d3c99e4bd35abc0e91f3ed58d5f33f7dfbd
Azeez <azeez@burhanitechnologies.com>
2023-11-17 19:57:28 +0530

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+
+export const mongoDBURL=
+'mongodb+srv://azizmh02hirani:vuUV9OpiwFmQ8vOU@book-store-aziz.yblusho.mongodb.net/books-collection?retryWrites=true&w=majority'
---
+++ backend/models/bookModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+export const bookSchema =mongoose.Schema(
+    {
+        title:{
+            type: 'string',
+            required: true,
+        },
+        author:{
+            type: 'string',
+            required: true,
+        },
+        publishYear:{
+            type:'number',
+            required:true
+    },
+},
+{
+    timestamps:true,
+}
+);
+export const Book = mongoose.model('Cat', bookSchema);
