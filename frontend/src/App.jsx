5e060085964a6d7ce657d91407529cea573431a6
Aziz <Azizmh02hirani@gmail.com>
2023-11-20 12:56:21 +0530

    frontedn trial 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes,Route} from 'react-router-dom'
+import {Routes,Route, BrowserRouter, HashRouter} from 'react-router-dom'
 import Home from './pages/Home'
 import CreateBook from './pages/CreateBook'
 import ShowBook from './pages/ShowBook'
@@ -10,15 +10,15 @@ import DeleteBook from './pages/DeleteBook'
 
 const App = () => {
   return (
-
+  <HashRouter basename='/'>
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route exact path='/' element={<Home />} />
       <Route path='/books/create' element={<CreateBook />} />
       <Route path="/books/details/:id" element={<ShowBook />} />
       <Route path="/books/edit/:id" element={<EditBook />} />
       <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
-    
+  </HashRouter>
   )
 }
 
